ba4874962201d491a820cdce6daed606e62ce6dd
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-02-16 15:56:49 +0530

    remove dist

--- src/index.js
+++ src/index.js
@@ -33,14 +33,6 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-  });
-}
-
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
