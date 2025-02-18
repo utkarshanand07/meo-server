e8bd1c372809032ee1b7594c6b192581458c8101
UtkarshAnand07 <utkarshanand7193@gmail.com>
2025-02-18 19:45:47 +0530

    socket fix

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "*",
+    origin: ["*", "http://localhost:5173"],
     credentials: true,
   })
 );
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "*"],
   },
 });
 
@@ -27,6 +27,9 @@ io.on("connection", (socket) => {
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
+  // Emit the socket.id to the connected user
+  socket.emit("connected", { socketId: socket.id });
+  
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
